1957632d216f37b690557dd19a2da51f345158fa
Shirshakkandel <64880594+Shirshakkandel@users.noreply.github.com>
2023-12-31 16:23:24 +0545

    Initial commit

---
+++ 6-unlimitedColors/chaiaurcode.js
@@ -0,0 +1,33 @@
+//generate a random color
+//1)function that create 6 random number from 0 to 16(0-F)
+//2)function changeBgColor
+//3)when function call in setInterval change
+
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+let intervalId;
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+
+  function changeBgColor() {
+    console.log(randomColor());
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
---
+++ README.md
@@ -0,0 +1,3 @@
+# dom-project-chaiaurcode-onzj8b
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/Shirshakkandel/dom-project-chaiaurcode-onzj8b)
